152fc56f4a9d09baa3702266ded3092e3a15e201
Chua Hui Shun <huishun@Chuas-MacBook-Pro.local>
2020-05-21 11:33:13 +0800

    fixed bug

--- app/src/main/java/com/secondwind/android/HomeFragment.java
+++ app/src/main/java/com/secondwind/android/HomeFragment.java
@@ -6,8 +6,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 public class HomeFragment extends Fragment {
 
@@ -18,13 +21,20 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+//        NavController navController = Navigation.findNavController(view);
 
         exerciseButton = (Button) view.findViewById(R.id.exerciseButton);
         exerciseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+//                navController.navigate(R.id.action_homeFragment_to_exerciseOneFragment);
             }
         });
+
     }
 }
