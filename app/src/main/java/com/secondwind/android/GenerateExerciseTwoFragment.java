ad9fa550111224b7edaa1e3ac18a2b0ed8d916ea
aaroiseverything <aaron@secondwindnation.com>
2020-06-03 11:05:23 +0800

    Update Generate Workout pages

--- app/src/main/java/com/secondwind/android/GenerateExerciseTwoFragment.java
+++ app/src/main/java/com/secondwind/android/GenerateExerciseTwoFragment.java
@@ -31,8 +31,9 @@ public class GenerateExerciseTwoFragment extends Fragment {
 
     NavController navController;
     private Button mDoneBtn;
-    private SeekBar seekbar2, seekBar3;
+    private SeekBar seekBar2, seekBar3;
     private CheckBox checkBox;
+    private TextView textView5, textView6;
     String firebaseKey;
 
 
@@ -72,13 +73,48 @@ public class GenerateExerciseTwoFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         navController = Navigation.findNavController(view);
         mDoneBtn = view.findViewById(R.id.doneBtn);
-
-
+        seekBar2 = view.findViewById(R.id.seekBar2);
+        seekBar3 = view.findViewById(R.id.seekBar3);
+        textView5 = view.findViewById(R.id.textView5);
+        textView6 = view.findViewById(R.id.textView6);
 
         mDoneBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 navController.navigate(R.id.action_generateExerciseTwoFragment_to_generateExerciseOneFragment);
+            }
+        });
+
+        seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView5.setText(""+progress+ " min");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView6.setText(""+Math.round(progress/10) + " /10");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
 
