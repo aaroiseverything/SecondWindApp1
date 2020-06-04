9d533f61512f52e1961e2e470a8452529feceac7
aaroiseverything <aaron@secondwindnation.com>
2020-06-04 16:51:07 +0800

    edit configuration data to firebase

--- app/src/main/java/com/secondwind/android/GenerateExerciseTwoFragment.java
+++ app/src/main/java/com/secondwind/android/GenerateExerciseTwoFragment.java
@@ -1,6 +1,7 @@
 package com.secondwind.android;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
@@ -23,7 +24,17 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 
 public class GenerateExerciseTwoFragment extends Fragment {
 
@@ -32,9 +43,15 @@ public class GenerateExerciseTwoFragment extends Fragment {
     NavController navController;
     private Button mDoneBtn;
     private SeekBar seekBar2, seekBar3;
-    private CheckBox checkBox;
+    private CheckBox checkBox1,checkBox2,checkBox3,checkBox4,checkBox5,checkBox6;
     private TextView textView5, textView6;
-    String firebaseKey;
+    private ArrayList<String> equipment;
+    private Integer timeAvailable, perceivedFatigue;
+    public String firebaseKey;
+    private DatabaseReference rref;
+    private FirebaseAuth mAuth;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
 
 
     private Button mResetBtn;
@@ -71,24 +88,98 @@ public class GenerateExerciseTwoFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        sharedPreferences = this.getContext().getSharedPreferences(getString(R.string.shared_prefs_name), this.getContext().MODE_PRIVATE);
+        //editor = sharedPreferences.edit();
+
+        rref = FirebaseDatabase.getInstance().getReference().child(getString(R.string.firebase_db_members));
+        firebaseKey = sharedPreferences.getString(getString(R.string.shared_prefs_key_firebasekey), "");
+
+        mAuth = FirebaseAuth.getInstance();
+
+        // Initalise list of goals
+        //Set<String> goalsSet = sharedPreferences.getStringSet("workoutConfiguration", new HashSet<>());
+        equipment = new ArrayList<>();
+
         navController = Navigation.findNavController(view);
         mDoneBtn = view.findViewById(R.id.doneBtn);
         seekBar2 = view.findViewById(R.id.seekBar2);
         seekBar3 = view.findViewById(R.id.seekBar3);
         textView5 = view.findViewById(R.id.textView5);
         textView6 = view.findViewById(R.id.textView6);
-
-        mDoneBtn.setOnClickListener(new View.OnClickListener() {
+        checkBox1 = view.findViewById(R.id.checkBox1);
+        checkBox2 = view.findViewById(R.id.checkBox2);
+        checkBox3 = view.findViewById(R.id.checkBox3);
+        checkBox4 = view.findViewById(R.id.checkBox4);
+        checkBox5 = view.findViewById(R.id.checkBox5);
+        checkBox6 = view.findViewById(R.id.checkBox6);
+        checkBox1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                navController.navigate(R.id.action_generateExerciseTwoFragment_to_generateExerciseOneFragment);
+                if(checkBox1.isChecked())
+                    equipment.add("No equipment");
+                else if (Arrays.asList(equipment).contains("No equipment"))
+                    equipment.remove("No equipment");
+
             }
         });
+        checkBox2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkBox2.isChecked())
+                    equipment.add("Home gym");
+                else if (Arrays.asList(equipment).contains("Home gym"))
+                    equipment.remove("Home gym");
+
+            }
+        });
+        checkBox3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkBox3.isChecked())
+                    equipment.add("Outdoor/ park gym");
+                else if (Arrays.asList(equipment).contains("Outdoor/ park gym"))
+                    equipment.remove("Outdoor/ park gym");
+
+            }
+        });
+        checkBox4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkBox4.isChecked())
+                    equipment.add("Small gym");
+                else if (Arrays.asList(equipment).contains("Small gym"))
+                    equipment.remove("Small gym");
+
+            }
+        });
+        checkBox5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkBox5.isChecked())
+                    equipment.add("Medium gym");
+                else if (Arrays.asList(equipment).contains("Medium gym"))
+                    equipment.remove("Medium gym");
+
+            }
+        });
+        checkBox6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkBox6.isChecked())
+                    equipment.add("Large gym");
+                else if (Arrays.asList(equipment).contains("Large gym"))
+                    equipment.remove("Large gym");
+
+            }
+        });
+
+
 
         seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 textView5.setText(""+progress+ " min");
+                timeAvailable = progress;
             }
 
             @Override
@@ -105,6 +196,7 @@ public class GenerateExerciseTwoFragment extends Fragment {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 textView6.setText(""+Math.round(progress/10) + " /10");
+                perceivedFatigue = Math.round(progress/10);
             }
 
             @Override
@@ -115,6 +207,21 @@ public class GenerateExerciseTwoFragment extends Fragment {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
 
+            }
+        });
+
+        mDoneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //push to firebase
+                //mDatabase=FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+                HashMap<String,Object> usermap = new HashMap<>(); // if you have data in diff data types go for HashMap<String,Object> or you can continue with HashMap<String,String>
+                usermap.put(firebaseKey  + "/workoutConfiguration"+"/timeAvailable", timeAvailable);
+                usermap.put(firebaseKey  + "/workoutConfiguration"+"/perceivedFatigue", perceivedFatigue);
+                usermap.put(firebaseKey  + "/workoutConfiguration"+"/equipment", equipment);
+                rref.updateChildren(usermap);
+                //back to fragment_generate_one
+                navController.navigate(R.id.action_generateExerciseTwoFragment_to_generateExerciseOneFragment);
             }
         });
 
